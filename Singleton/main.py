6524d3a7e66d2c5a1996afc08d810bc5253ebd75
関田悠 <sekitayuu@Yus-MacBook-Pro-2.local>
2021-02-20 14:43:27 +0700

    fix singleton

--- Singleton/main.py
+++ Singleton/main.py
@@ -3,9 +3,11 @@ from singleton import MySingleton
 
 
 if __name__ == '__main__':
-    s1 = MySingleton()
-    s2 = MySingleton()
+    s1 = MySingleton.get_instance()
+    s2 = MySingleton.get_instance()
     s3 = MySingleton.get_instance()
+    s4 = MySingleton()
     print('s1 == s2 ? ', s1 == s2)
     print('s2 == s3 ? ', s2 == s3)
     print('s1 == s3 ? ', s1 == s3)
+    print('s1 == s4 ? ', s1 == s4)
